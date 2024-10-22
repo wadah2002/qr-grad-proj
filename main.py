bf226bfff179e536138a7ce43969f36514ade451
wadam-m <wadahx052@gmail.com>
2024-10-22 12:25:46 +0300

    Get text from the browser and generate qr code

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
+from qr2 import generateQRcode
 
 app = Flask(__name__)
 app.config.update(TEMPLATES_AUTO_RELOAD=True)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index_page():
+    if request.method == "POST":
+        data = request.form.get("dataInput")
+        if data:
+            generateQRcode(data, "./static/sample.png")
     return render_template("index.html", active="text")
 
 @app.route("/url")
